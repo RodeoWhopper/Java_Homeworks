7a3bc76825ef6cde5485ba085b77ac32ef40d252
Emirhan Bulut <emirhanblt9@gmail.com>
2023-02-19 17:35:38 +0300

    Fixture Creater has been added

---
+++ Fixture/Main.java
@@ -0,0 +1,11 @@
+package Fixture;
+
+import Fixture.Fikstür;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Fikstür fixtureCreater=new Fikstür();
+        fixtureCreater.run();
+    }
+}
